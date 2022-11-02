07c8cf69b6a7f098657a5902af031361eebea470
Sithara-tech <sitharakumarasingha@gmail.com>
2022-11-02 22:16:41 +0530

    report driver and route schedule

--- frontend/webclient/src/pages/Reports/views/index.jsx
+++ frontend/webclient/src/pages/Reports/views/index.jsx
@@ -1,7 +1,110 @@
+import { Button } from "@mui/material";
 import React from "react";
+import RouteScheduleAPI from "../../../core/services/RouteScheduleAPI";
+import DrivesAPI from "../../../core/services/DrivesAPI";
+import RouteScheduleReportGenerator from "./RouteSheduleReport";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Grid, CardActionArea, CardActions } from '@mui/material';
+import DriversReportGenerator from "./Drivers";
 
 const ReportPage = () => {
-  return <>ReportPage</>;
+  const [routeSchedules, setRouteSchedules] = React.useState([]);
+  async function fetchRouteScheduleData() {
+    const response = await RouteScheduleAPI.getAll();
+    if (response.status === 200) {
+      setRouteSchedules(response.data.data);
+    }
+  }
+
+  const [drivers, setDrivers] = React.useState([]);
+  async function fetchDriversData() {
+    const response = await DrivesAPI.getAll();
+    if (response.status === 200) {
+      setDrivers(response.data.data);
+    }
+  }
+
+
+  React.useEffect(() => {
+    fetchRouteScheduleData()
+    fetchDriversData()
+  }, [])
+
+  return <>
+    <Grid
+      container
+      direction="row"
+      justifyContent="space-evenly"
+      alignItems="center"
+      columns={13}
+    ><Grid item xs={3}>
+    <Card sx={{ maxWidth: 300 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="core/images/image"
+          alt="Route Schedule"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Route Schedule Report
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            You can get report for route Schedule Report
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+            <Button size="small" color="primary" onClick={() => RouteScheduleReportGenerator(routeSchedules)}>Download</Button>  
+      </CardActions> 
+        </Card></Grid><Grid item xs={3}>
+      <Card sx={{ maxWidth: 300 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image="core/images/image"
+            alt="Route Schedule"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Drivers Report
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              You can get report for route Schedule Report
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+            <Button size="small" color="primary" onClick={() => DriversReportGenerator(drivers)}>Download</Button>
+        </CardActions>
+        </Card></Grid><Grid item xs={3}>
+      <Card sx={{ maxWidth: 300 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image="core/images/image"
+            alt="Route Schedule"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Inspectors Report
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              You can get report for route Schedule Report
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary" onClick={() => RouteScheduleReportGenerator(routeSchedules)}>Download</Button>
+        </CardActions>
+        </Card></Grid></Grid>
+  </>;
 };
 
 export default ReportPage;
